e9e4cb3eaa1efde4899b20bb43fb80d9c13b9680
Diego Castro <dieg_evw@hotmail.com>
2022-06-05 17:35:29 -0500

    adds k8s service for load balancer, and updates code to work with hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const clienApi = ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.diegh-dev.xyz',
       headers: req.headers,
     });
   } else {
